282c01bc46487ea3b36341098bfad1098edb1fcd
samuelsze <54306877+szesam@users.noreply.github.com>
2021-02-17 19:41:10 -0500

    fishtank.c

---
+++ quest-1/code/fishtank.c
@@ -0,0 +1,436 @@
+// Standard C library
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+// GPIO library
+#include "driver/gpio.h"
+
+// RTOS tasks library
+#include "esp_types.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+// Timer library
+#include "driver/periph_ctrl.h"
+#include "driver/timer.h"
+
+// i2c library
+#include "driver/i2c.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+// Console IO library
+#include "driver/uart.h"
+#include "sdkconfig.h"
+#include "esp_vfs_dev.h"
+
+// Timer definition
+#define TIMER_DIVIDER         16    //  Hardware timer clock divider
+#define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // to seconds
+#define TIMER_INTERVAL_SEC   (1)    // Sample test interval for the first timer
+#define TEST_WITH_RELOAD      1     // Testing will be done with auto reload
+
+// 14-Segment Display
+#define SLAVE_ADDR                         0x70 // alphanumeric address
+#define OSC                                0x21 // oscillator cmd
+#define HT16K33_BLINK_DISPLAYON            0x01 // Display on cmd
+#define HT16K33_BLINK_OFF                  0    // Blink off cmd
+#define HT16K33_BLINK_CMD                  0x80 // Blink cmd
+#define HT16K33_CMD_BRIGHTNESS             0xE0 // Brightness cmd
+
+// Master I2C
+#define I2C_EXAMPLE_MASTER_SCL_IO          22   // gpio number for i2c clk
+#define I2C_EXAMPLE_MASTER_SDA_IO          23   // gpio number for i2c data
+#define I2C_EXAMPLE_MASTER_NUM             I2C_NUM_0  // i2c port
+#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE  0    // i2c master no buffer needed
+#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE  0    // i2c master no buffer needed
+#define I2C_EXAMPLE_MASTER_FREQ_HZ         100000     // i2c master clock freq
+#define WRITE_BIT                          I2C_MASTER_WRITE // i2c master write
+#define READ_BIT                           I2C_MASTER_READ  // i2c master read
+#define ACK_CHECK_EN                       true // i2c master will check ack
+#define ACK_CHECK_DIS                      false// i2c master will not check ack
+#define ACK_VAL                            0x00 // i2c ack value
+#define NACK_VAL                           0xFF // i2c nack value
+
+
+//global variable for user input time.
+uint16_t user_time;
+//global variable for time manipulation in led, timer and servo
+uint16_t time_counter;
+// A simple structure to pass "events" to main task
+typedef struct {
+    int flag;     // flag for enabling stuff in main code
+} timer_event_t;
+
+// Initialize queue handler for timer-based events
+xQueueHandle timer_queue;
+
+// ISR handler
+void IRAM_ATTR timer_group0_isr(void *para) {
+    // int timer_idx = (int) para;
+    // uint64_t timer_counter_value = timer_group_get_counter_value_in_isr(TIMER_GROUP_0, timer_idx);
+    // Prepare basic event data, aka set flag
+    timer_event_t evt;
+    // evt.timer_group = 0;
+    evt.flag = 1;
+    // evt.timer_idx = timer_idx;
+    // evt.timer_counter_value = timer_counter_value;
+    // Clear the interrupt, Timer 0 in group 0
+    TIMERG0.int_clr_timers.t0 = 1;
+
+    // After the alarm triggers, we need to re-enable it to trigger it next time
+    TIMERG0.hw_timer[TIMER_0].config.alarm_en = TIMER_ALARM_EN;
+
+    // Send the event data back to the main program task
+    xQueueSendFromISR(timer_queue, &evt, NULL);
+}
+
+// Function to initiate i2c -- note the MSB declaration!
+static void i2c_example_master_init(){
+    // Debug
+    printf("\n>> i2c Config\n");
+    int err;
+
+    // Port configuration
+    int i2c_master_port = I2C_EXAMPLE_MASTER_NUM;
+
+    /// Define I2C configurations
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;                              // Master mode
+    conf.sda_io_num = I2C_EXAMPLE_MASTER_SDA_IO;              // Default SDA pin
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;                  // Internal pullup
+    conf.scl_io_num = I2C_EXAMPLE_MASTER_SCL_IO;              // Default SCL pin
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;                  // Internal pullup
+    conf.master.clk_speed = I2C_EXAMPLE_MASTER_FREQ_HZ;       // CLK frequency
+    err = i2c_param_config(i2c_master_port, &conf);           // Configure
+    if (err == ESP_OK) {printf("- parameters: ok\n");}
+
+    // Install I2C driver
+    err = i2c_driver_install(i2c_master_port, conf.mode,
+                       I2C_EXAMPLE_MASTER_RX_BUF_DISABLE,
+                       I2C_EXAMPLE_MASTER_TX_BUF_DISABLE, 0);
+    // i2c_set_data_mode(i2c_master_port,I2C_DATA_MODE_LSB_FIRST,I2C_DATA_MODE_LSB_FIRST);
+    if (err == ESP_OK) {printf("- initialized: yes\n\n");}
+
+    // Dat in MSB mode
+    i2c_set_data_mode(i2c_master_port, I2C_DATA_MODE_MSB_FIRST, I2C_DATA_MODE_MSB_FIRST);
+}
+
+// Turn on oscillator for alpha display
+int alpha_oscillator() {
+  int ret;
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+  i2c_master_write_byte(cmd, OSC, ACK_CHECK_EN);
+  i2c_master_stop(cmd);
+  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+  i2c_cmd_link_delete(cmd);
+  vTaskDelay(200 / portTICK_RATE_MS);
+  return ret;
+}
+
+// Set blink rate to off
+int no_blink() {
+  int ret;
+  i2c_cmd_handle_t cmd2 = i2c_cmd_link_create();
+  i2c_master_start(cmd2);
+  i2c_master_write_byte(cmd2, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+  i2c_master_write_byte(cmd2, HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (HT16K33_BLINK_OFF << 1), ACK_CHECK_EN);
+  i2c_master_stop(cmd2);
+  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd2, 1000 / portTICK_RATE_MS);
+  i2c_cmd_link_delete(cmd2);
+  vTaskDelay(200 / portTICK_RATE_MS);
+  return ret;
+}
+
+// Set Brightness
+int set_brightness_max(uint8_t val) {
+  int ret;
+  i2c_cmd_handle_t cmd3 = i2c_cmd_link_create();
+  i2c_master_start(cmd3);
+  i2c_master_write_byte(cmd3, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+  i2c_master_write_byte(cmd3, HT16K33_CMD_BRIGHTNESS | val, ACK_CHECK_EN);
+  i2c_master_stop(cmd3);
+  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd3, 1000 / portTICK_RATE_MS);
+  i2c_cmd_link_delete(cmd3);
+  vTaskDelay(200 / portTICK_RATE_MS);
+  return ret;
+}
+
+// Initialize timer 0 in group 0 for 1 sec alarm interval and auto reload
+static void init() {
+    
+    //initialize i2c
+    i2c_example_master_init();
+
+    /* Select and initialize basic parameters of the timer */
+    timer_config_t config;
+    config.divider = TIMER_DIVIDER;
+    config.counter_dir = TIMER_COUNT_UP;
+    config.counter_en = TIMER_PAUSE;
+    config.alarm_en = TIMER_ALARM_EN;
+    config.intr_type = TIMER_INTR_LEVEL;
+    config.auto_reload = TEST_WITH_RELOAD;
+    timer_init(TIMER_GROUP_0, TIMER_0, &config);
+
+    // Timer's counter will initially start from value below
+    timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0x00000000ULL);
+
+    // Configure the alarm value and the interrupt on alarm
+    timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, TIMER_INTERVAL_SEC * TIMER_SCALE);
+    timer_enable_intr(TIMER_GROUP_0, TIMER_0);
+    timer_isr_register(TIMER_GROUP_0, TIMER_0, timer_group0_isr,
+        (void *) TIMER_0, ESP_INTR_FLAG_IRAM, NULL);
+
+    // Start timer
+    timer_start(TIMER_GROUP_0, TIMER_0);
+
+}
+
+
+// Console Io to get user input time
+static void gettime()
+{
+    int hours;
+    int minutes;
+    while(1)
+    {
+        printf("Enter hours desired for fish feeding: \n");
+        if(scanf("%d", &hours) == 1)
+        {
+            printf("Enter minutes desired for fish feeding: \n");
+            if(scanf("%d", &minutes) == 1)
+            {
+                // do some conversion from hour:minutes into seconds.
+                user_time = (uint16_t)(hours * 3600 + minutes * 60);
+                printf("%d\n",user_time);
+                time_counter = user_time;
+                break;
+            }
+            
+        else
+        {
+            // user entered a non-integer
+            printf("Wrong input, please enter again \n");
+            while((getchar()) != '\n');
+            continue;
+        }
+    }
+}
+// The TIMER of this example program
+static void timer_evt_task(void *arg) {
+    while (1) {
+        // Create dummy structure to store structure from queue
+        timer_event_t evt;
+        // Transfer from queue
+        xQueueReceive(timer_queue, &evt, portMAX_DELAY);
+        if (evt.flag == 1)
+        {
+            time_counter--;
+        }
+    }
+}
+
+static void i2c_task() //turn on alphanumeric display
+{
+    int ret;
+    ret = alpha_oscillator();
+    if(ret == ESP_OK) {printf("- oscillator: ok \n");}
+    ret = no_blink();
+    if(ret == ESP_OK) {printf("- blink: off \n");}
+    ret = set_brightness_max(0xF);
+    if(ret == ESP_OK) {printf("- brightness: max \n");}
+
+    // Write to characters to buffer
+    uint16_t displaybuffer[8];
+    static const uint16_t alphafonttable[] = 
+    {
+    0b0000000000000001, 0b0000000000000010, 0b0000000000000100,
+    0b0000000000001000, 0b0000000000010000, 0b0000000000100000,
+    0b0000000001000000, 0b0000000010000000, 0b0000000100000000,
+    0b0000001000000000, 0b0000010000000000, 0b0000100000000000,
+    0b0001000000000000, 0b0010000000000000, 0b0100000000000000,
+    0b1000000000000000, 0b0000000000000000, 0b0000000000000000,
+    0b0000000000000000, 0b0000000000000000, 0b0000000000000000,
+    0b0000000000000000, 0b0000000000000000, 0b0000000000000000,
+    0b0001001011001001, 0b0001010111000000, 0b0001001011111001,
+    0b0000000011100011, 0b0000010100110000, 0b0001001011001000,
+    0b0011101000000000, 0b0001011100000000,
+    0b0000000000000000, //
+    0b0000000000000110, // !
+    0b0000001000100000, // "
+    0b0001001011001110, // #
+    0b0001001011101101, // $
+    0b0000110000100100, // %
+    0b0010001101011101, // &
+    0b0000010000000000, // '
+    0b0010010000000000, // (
+    0b0000100100000000, // )
+    0b0011111111000000, // *
+    0b0001001011000000, // +
+    0b0000100000000000, // ,
+    0b0000000011000000, // -
+    0b0000000000000000, // .
+    0b0000110000000000, // /
+    0b0000110000111111, // 0
+    0b0000000000000110, // 1
+    0b0000000011011011, // 2
+    0b0000000010001111, // 3
+    0b0000000011100110, // 4
+    0b0010000001101001, // 5
+    0b0000000011111101, // 6
+    0b0000000000000111, // 7
+    0b0000000011111111, // 8
+    0b0000000011101111, // 9
+    0b0001001000000000, // :
+    0b0000101000000000, // ;
+    0b0010010000000000, // <
+    0b0000000011001000, // =
+    0b0000100100000000, // >
+    0b0001000010000011, // ?
+    0b0000001010111011, // @
+    0b0000000011110111, // A
+    0b0001001010001111, // B
+    0b0000000000111001, // C
+    0b0001001000001111, // D
+    0b0000000011111001, // E
+    0b0000000001110001, // F
+    0b0000000010111101, // G
+    0b0000000011110110, // H
+    0b0001001000000000, // I
+    0b0000000000011110, // J
+    0b0010010001110000, // K
+    0b0000000000111000, // L
+    0b0000010100110110, // M
+    0b0010000100110110, // N
+    0b0000000000111111, // O
+    0b0000000011110011, // P
+    0b0010000000111111, // Q
+    0b0010000011110011, // R
+    0b0000000011101101, // S
+    0b0001001000000001, // T
+    0b0000000000111110, // U
+    0b0000110000110000, // V
+    0b0010100000110110, // W
+    0b0010110100000000, // X
+    0b0001010100000000, // Y
+    0b0000110000001001, // Z
+    0b0000000000111001, // [
+    0b0010000100000000, //
+    0b0000000000001111, // ]
+    0b0000110000000011, // ^
+    0b0000000000001000, // _
+    0b0000000100000000, // `
+    0b0001000001011000, // a
+    0b0010000001111000, // b
+    0b0000000011011000, // c
+    0b0000100010001110, // d
+    0b0000100001011000, // e
+    0b0000000001110001, // f
+    0b0000010010001110, // g
+    0b0001000001110000, // h
+    0b0001000000000000, // i
+    0b0000000000001110, // j
+    0b0011011000000000, // k
+    0b0000000000110000, // l
+    0b0001000011010100, // m
+    0b0001000001010000, // n
+    0b0000000011011100, // o
+    0b0000000101110000, // p
+    0b0000010010000110, // q
+    0b0000000001010000, // r
+    0b0010000010001000, // s
+    0b0000000001111000, // t
+    0b0000000000011100, // u
+    0b0010000000000100, // v
+    0b0010100000010100, // w
+    0b0010100011000000, // x
+    0b0010000000001100, // y
+    0b0000100001001000, // z
+    0b0000100101001001, // {
+    0b0001001000000000, // |
+    0b0010010010001001, // }
+    0b0000010100100000, // ~
+    0b0011111111111111,
+    };
+    // Continually writes the same command
+    while (1) {
+        timer_event_t evt;
+        // Transfer from queue
+        xQueueReceive(timer_queue, &evt, portMAX_DELAY);
+        // Create dummy structure to store structure from queue
+        if (time_counter == 0) 
+        {
+            // Display feed on LED
+            displaybuffer[3] = alphafonttable['D'];
+            displaybuffer[2] = alphafonttable['E'];
+            displaybuffer[1] = alphafonttable['E'];
+            displaybuffer[0] = alphafonttable['F'];
+            // Send commands characters to display over I2C
+            i2c_cmd_handle_t cmd4 = i2c_cmd_link_create();
+            i2c_master_start(cmd4);
+            i2c_master_write_byte(cmd4, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+            i2c_master_write_byte(cmd4, (uint8_t)0x00, ACK_CHECK_EN);
+            for (uint8_t i=0; i<8; i++) {
+                i2c_master_write_byte(cmd4, displaybuffer[i] & 0xFF, ACK_CHECK_EN);
+                i2c_master_write_byte(cmd4, displaybuffer[i] >> 8, ACK_CHECK_EN);
+            }
+            i2c_master_stop(cmd4);
+            ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd4, 1000 / portTICK_RATE_MS);
+            i2c_cmd_link_delete(cmd4);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            // Rotate servo. pause timer during server rotation. 
+            // reset time_counter to user inputted time again
+            time_counter = user_time;
+        }
+        if (evt.flag == 1)
+        {
+            int h = time_counter/3600;
+            int m = (time_counter - (3600*h))/60;
+            int s = (time_counter - (3600*h) - (m*60));
+            displaybuffer[3] = alphafonttable[m%10 + '0'];
+            displaybuffer[2] = alphafonttable[((m/10)%10) + '0'];
+            displaybuffer[1] = alphafonttable[h%10 + '0'];
+            displaybuffer[0] = alphafonttable[((h/10)%10) + '0'];
+            printf("Remaining time: %d hours, %d minutes, %d seconds\n", h,m,s);
+            // Send commands characters to display over I2C
+            i2c_cmd_handle_t cmd4 = i2c_cmd_link_create();
+            i2c_master_start(cmd4);
+            i2c_master_write_byte(cmd4, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+            i2c_master_write_byte(cmd4, (uint8_t)0x00, ACK_CHECK_EN);
+            for (uint8_t i=0; i<8; i++) {
+                i2c_master_write_byte(cmd4, displaybuffer[i] & 0xFF, ACK_CHECK_EN);
+                i2c_master_write_byte(cmd4, displaybuffer[i] >> 8, ACK_CHECK_EN);
+            }
+            i2c_master_stop(cmd4);
+            ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd4, 1000 / portTICK_RATE_MS);
+            i2c_cmd_link_delete(cmd4);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+       
+    }
+
+}
+
+void app_main(void) {
+    // Create a FIFO queue for timer-based
+    timer_queue = xQueueCreate(10, sizeof(timer_event_t));
+
+    // Console IO to get user inputted time 
+    // initialize uart driver for console io
+    ESP_ERROR_CHECK( uart_driver_install(UART_NUM_0,
+    256, 0, 0, NULL, 0) );
+    esp_vfs_dev_uart_use_driver(UART_NUM_0);
+    gettime();
+    // Initiate alarm using timer API, initialize i2c,
+    init();
+    // Create task to handle timer-based events
+    xTaskCreate(timer_evt_task, "timer_evt_task", 4096, NULL, configMAX_PRIORITIES, NULL);
+    xTaskCreate(i2c_task, "i2c_task", 4096, NULL, configMAX_PRIORITIES-1, NULL);
+
+
+}
