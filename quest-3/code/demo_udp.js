764331dfab5c64a6425350db437c68894734617f
Carmen Hurtado <carmenhg@bu.edu>
2021-03-20 14:56:52 -0400

    Pi camera video embedded

--- quest-3/code/demo_udp.js
+++ quest-3/code/demo_udp.js
@@ -8,7 +8,7 @@ var voltage =[], temp = [], xaccel = [], yaccel = [], zaccel = [], roll = [], pi
 //create stream to write sensor data into csv file called out.csv in same directory.
 var fs = require('fs')
 var stream = fs.createWriteStream("out.csv");
-stream.write("Voltage(mV), Thermistor(C), Ultrasonic(m), IR(m)\n")
+stream.write("Voltage(mV), Xaccel, Yaccel, Zaccel, Roll, Pitch, Thermistor(C)\n")
 
 //Create acknowledge packet to send back to udp_client upon message received
 const msg = Buffer.from('Acknowledged');
@@ -24,6 +24,7 @@ server.on('message', function (message, remote) {
         console.log(remote.address + ':' + remote.port +' - ' + message.toString());
         server.send(msg, remote.port, remote.address);
         push_sensor_data(message);
+        stream.write(String(message));
 });
 // i is x-axis
 var i = 0; 
@@ -31,11 +32,45 @@ function push_sensor_data(data)
 {
     data = data.toString();
     data = data.split(",");
+    i = i + 1;
     voltage = data[0];
-    i = i + 0.5;
+    xaccel.push({
+		x: parseInt(i),
+		y: parseFloat(data[1])
+	});
+	yaccel.push({
+		x: parseInt(i),
+		y: parseFloat(data[2])
+	});
+	zaccel.push({
+		x: parseInt(i),
+		y: parseFloat(data[3])
+    });
+    roll.push({
+		x: parseInt(i),
+		y: parseFloat(data[4])
+	});
+	pitch.push({
+		x: parseInt(i),
+		y: parseFloat(data[5])
+    });
+    temp.push({
+		x: parseInt(i),
+		y: parseFloat(data[6])
+	});
+    if (temp.length > 15) //number of data poitns visible at any time
+    {
+        temp.shift();
+        xaccel.shift();
+        yaccel.shift();
+        zaccel.shift();
+        roll.shift();
+        pitch.shift();
+
+    }
 }
 //Bind server to PI local address on router with port at 3333
-server.bind(3333,'192.168.1.136');
+server.bind(3333,'192.168.1.116');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // socket io script connect server to client (web browser) to display canvasjs charts.
@@ -48,15 +83,109 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+// first chart format
+var chartOptions1 = {
+	title:{
+		text: "Vibration data"
+	},
+	axisX: {
+		Prefix: "Seconds",
+		title: "Time(sec)",
+		interval: 1
+	},
+	axisY: {
+		title: "Distance",
+		lineColor: "#7F6084",
+		tickColor: "#7F6084",
+		labelFontColor: "#7F6084",
+		titleFontColor: "#7F6084",
+		includeZero: true,
+		suffix: "m",
+	},
+	legend: {
+		cursor: "pointer",
+		verticalAlign: "bottom",
+		horizontalAlign: "center",
+		dockInsidePlotArea: true,
+	},
+	toolTip: {
+		shared: true
+	},
+	data: [
+	{
+		name: "X acceleration",
+		type: "spline",
+		color: "#7F6084",
+		yValueFormatString: "0.##m",
+		showInLegend: true,
+		dataPoints: xaccel,
+	},
+	{
+		name: "Y acceleration",
+		type: "spline",
+		markerType: "cross",
+		color: "#ff1a1a",
+		yValueFormatString: "0.##m",
+		showInLegend: true,
+		dataPoints: yaccel,
+	}]
+
+};
+
+//second chart with temperature sensor
+var chartOptions2 = {
+	title:{
+		text: "Temperature Sensor data"
+	},
+	axisX: {
+		Prefix: "Seconds",
+		title: "Time(sec)",
+		interval: 1
+	},
+	axisY:[
+	{
+		title: "Temperature",
+		lineColor: "#369EAD",
+		tickColor: "#369EAD",
+		labelFontColor: "#369EAD",
+		titleFontColor: "#369EAD",
+		includeZero: true,
+		suffix: "C"
+	}],
+	legend: {
+		cursor: "pointer",
+		verticalAlign: "bottom",
+		horizontalAlign: "center",
+		dockInsidePlotArea: true,
+	},
+	toolTip: {
+		shared: true
+	},
+	data: [
+	{
+		name: "temperature",
+		type: "spline",
+		color: "#369EAD",
+		yValueFormatString: "####C",
+		showInLegend: true,
+		dataPoints: temp,
+		axisYIndex:1
+	},
+	]
+
+};
 // emit data every 1second
 setInterval(function(){
+	io.emit('dataMsg1', chartOptions1);
+	io.emit('dataMsg2', chartOptions2);
 	io.emit('dataMsg3',	voltage);
  }, 1000);
 
-
-//Socket io on user connection
+//socket io creation. 
 io.on('connection', function(socket){
-	console.log('a user connected');
+	//console.log('a user connected');
+	io.emit('dataMsg1', chartOptions1);
+	io.emit('dataMsg2', chartOptions2);
 	io.emit('dataMsg3', voltage);
 });
 
